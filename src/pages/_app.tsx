c6ccdd130a94bcb3ea2a818951f9e7cc0944d0fe
Samuel <samuelpl0888@gmail.com>
2023-03-25 02:17:16 -0400

    Add viewport meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,25 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import Layout from "~/components/Layout";
-
+import Head from "next/head";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>Samuel Palacios | Full Stack Developer</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Samuel Palacios is a full stack developer and software engineering student who loves building web apps with exceptional user experiences."
+        />
+
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,11 @@
 import { type NextPage } from "next";
-import dynamic from "next/dynamic";
-import Head from "next/head";
 import Hero from "../components/Hero";
+import dynamic from "next/dynamic";
 const MainContent = dynamic(() => import("../components/MainContent"));
 
 const Home: NextPage = () => {
   return (
     <>
-      <Head>
-        <title>Samuel Palacios | Full Stack Developer</title>
-        <meta
-          name="description"
-          content="Samuel Palacios is a full stack developer and software engineering student who loves building web apps with exceptional user experiences."
-        />
-
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <Hero />
       <MainContent />
     </>
